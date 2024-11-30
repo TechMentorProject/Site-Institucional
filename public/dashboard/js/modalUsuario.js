ce34d019272aee60b7848c6da0cc94b9f57af869
LCarminatti <lucas.csantos@sptech.school>
2024-11-30 00:25:54 -0300

    feature add button addUser

--- public/dashboard/js/modalUsuario.js
+++ public/dashboard/js/modalUsuario.js
@@ -8,9 +8,13 @@ const editModalContainer = document.getElementById('edit-modal-container');
 const saveEditButton = document.getElementById('save-edit');
 const cancelEditButton = document.getElementById('cancel-edit');
 
+const addUserButton = document.getElementById('add-user-btn');
+const userModalContainer = document.getElementById('user-modal-container');
+const cancelButton = document.getElementById('cancel-btn');
+const userForm = document.getElementById('user-form');
+
 
 let currentUser = null;
-
 let userToDelete = null;
 
 deleteButtons.forEach((button, index) => {
@@ -36,21 +40,37 @@ cancelDelete.addEventListener('click', () => {
 editButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
         console.log("Editando usuário");
-        // Exibe o modal
         editModalContainer.style.display = 'flex';
     });
 });
 
-// Evento de cancelar a edição
 cancelEditButton.addEventListener('click', () => {
-    editModalContainer.style.display = 'none'; // Oculta o modal
-});
-
-// Evento de salvar as alterações
-saveEditButton.addEventListener('click', (e) => {
-    e.preventDefault(); // Impede o envio do formulário
-    // Feche o modal
     editModalContainer.style.display = 'none';
 });
 
+saveEditButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    editModalContainer.style.display = 'none';
+});
+
+addUserButton.addEventListener('click', () => {
+    userModalContainer.style.display = 'flex';
+});
+
+cancelButton.addEventListener('click', () => {
+    userModalContainer.style.display = 'none';
+});
+
+userForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const cpf = document.getElementById('cpf').value;
+    const email = document.getElementById('email').value;
+    const cargo = document.getElementById('cargo').value;
+
+    console.log({ nome, cpf, email, cargo });
+
+    userModalContainer.style.display = 'none';
+});
 
